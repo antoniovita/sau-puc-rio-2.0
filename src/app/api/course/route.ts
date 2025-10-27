8337fc7bf5301f21d13c060be0b3e1b4b9373dc9
Maria (Malu) Luiza <148253498+marialuiza15@users.noreply.github.com>
2025-10-27 00:45:12 +0000

    refactor: corrigindo rota course

--- src/app/api/course/route.ts
+++ src/app/api/course/route.ts
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 
 /**
  * GET /api/course
- * Retorna todas as turmas (courses) com subject e professor associados
+ * Retorna todas as turmas  com materias e professor associados
  */
 export async function GET() {
   try {
@@ -29,33 +29,30 @@ export async function GET() {
  * POST /api/course
  * Cria uma nova turma
  */
+    
 export async function POST(request: Request) {
   try {
     const body = await request.json();
-    const {
-      code,
-      periodo,
-      room,
-      vagas,
-      startTime,
-      endTime,
-      days,
-      subjectId,
-      professorId,
-    } = body;
+    const { code, name, subjectIds } = body; //subjectIds será um conjuntos de materias obrigatorias que compoem o curso
 
-    const newCourse = await prisma.group.create({
+    // Criação do curso
+    const newCourse = await prisma.course.create({
       data: {
         code,
-        periodo,
-        room,
-        vagas,
-        startTime,
-        endTime,
-        days,
-        subjectId,
-        professorId,
+        name,
       },
+    });
+
+    // cada subjectId cria um relacionamento na tabela CourseSubject
+    const courseSubjects = subjectIds.map((subjectId: string) => ({
+      courseId: newCourse.id,
+      subjectId,
+      type, 
+    }));
+
+    // relacionamentos entre Curso e Subjects
+    await prisma.courseSubject.createMany({
+      data: courseSubjects,
     });
 
     return NextResponse.json(newCourse, { status: 201 });
